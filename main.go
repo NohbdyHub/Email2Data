e72964f3eaaba483dd28ef3f68e28b994bdeee19
Nico <NicolasCarty03@gmail.com>
2026-01-30 11:19:11 -0500

    Add simple JSON configuration, now use cgo to embed wkhtmltopdf instead of running system binary

--- main.go
+++ main.go
@@ -3,8 +3,12 @@ package main
 import (
 	"context"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
-	"log"
+	"io"
+	"time"
+
+	//	"log"
 	"os"
 	"os/exec"
 	"path"
@@ -20,10 +24,37 @@ import (
 	"google.golang.org/api/option"
 )
 
+type GmailQuery struct {
+	Sender string
+	Date string
+	Subject string
+}
+
+func (q *GmailQuery) String() string {
+	var b strings.Builder
+
+	var t time.Time
+	if q.Date == "auto" {
+		t = time.Now()
+		t = time.Date(t.Year(), t.Month(), 1, 0, 0, 0, 0, t.Location())
+	} else {
+		t = Must(time.Parse("01/2006", q.Date))
+	}
+
+	b.WriteString("from:")
+	b.WriteString(q.Sender)
+	b.WriteString(" after:")
+	b.WriteString(t.Format("2006/01/02"))
+	b.WriteString(" before:")
+	b.WriteString(t.AddDate(0, 1, 0).Format("2006/01/02"))
+	b.WriteString(" subject:")
+	b.WriteString("\"" + q.Subject + "\"")
+
+	return b.String()
+}
+
 var mailService *gmail.Service
-var sender string
-var date string
-var subject string
+var query = GmailQuery{}
 
 func Must[T any](obj T, err error) T {
 	if err != nil {
@@ -43,18 +74,33 @@ func getHeader(h []*gmail.MessagePartHeader, n string) string {
 
 func init() {
 	pdf.DebugOn = true
-	sender = "noreply@premiumparking.com"
-	date = "2026/01/01"
-	subject = "\"Expired\""
 
-	// google auth setup
-	b, err := os.ReadFile("credentials.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+	queryFile := Must(os.OpenFile("query.json", os.O_CREATE | os.O_RDWR, 0666))
+	defer queryFile.Close()
+
+	queryJSON := Must(io.ReadAll(queryFile))
+
+	if len(queryJSON) == 0 {
+		query = GmailQuery{Sender: "noreply@premiumparking.com", Date: "auto", Subject: "Expired"}
+		queryFile.Write(Must(json.MarshalIndent(query, "", "	")))
+		fmt.Println("Please ensure `query.json` has the correct information")
+		fmt.Println("The default query will find all PremiumParking receipts from the current month")
+		fmt.Println("Sender: Email address you recieve parking confirmations from")
+		fmt.Println("Date: \"auto\" to use current realtime month, MM/YYYY to filter to a specific month")
+		fmt.Println("Subject: Only retrieve mail including this phrase in the Subject line")
+		os.Exit(0)
+	} else {
+		err := json.Unmarshal(queryJSON, &query)
+		if err != nil {
+			panic(err)
+		}
 	}
 
-	config := Must(google.ConfigFromJSON(b, gmail.GmailReadonlyScope))
-	client := auth.GetClient(config)
+	// google auth setup
+	b := Must(os.ReadFile("credentials.json"))
+
+	oauth := Must(google.ConfigFromJSON(b, gmail.GmailReadonlyScope))
+	client := auth.GetClient(oauth)
 	mailService = Must(gmail.NewService(context.Background(), option.WithHTTPClient(client)))
 }
 
@@ -65,14 +111,7 @@ func main() {
 	fmt.Println(t)
 
 	// retrieve all mail from {sender} that was received after {date}, only including mail containing {subject} in the Subject line
-	var b strings.Builder
-	b.WriteString("from:")
-	b.WriteString(sender)
-	b.WriteString(" after:")
-	b.WriteString(date)
-	b.WriteString(" subject:")
-	b.WriteString(subject)
-	msgs := Must(mailService.Users.Messages.List("me").Q(b.String()).Do())
+	msgs := Must(mailService.Users.Messages.List("me").Q(query.String()).Do())
 	for _, msg := range msgs.Messages {
 		email := Must(mailService.Users.Messages.Get("me", msg.Id).Format("full").Do())
 
